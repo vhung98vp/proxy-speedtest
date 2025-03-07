66bb5e470bae7a8f9fd0026a739a9a71c5f9a052
Phùng Hùng <vhung98vp@gmail.com>
2025-03-07 11:32:59 +0700

    Add internet working count

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ results = test_all_proxies()
 ip_count = sum(map(lambda x: 1 if re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', x['ip']) else 0, results))
 speed_count = sum(map(lambda x: 1 if x['speed_result']['download_speed'] != 'N/A' else 0, results))
 search_count = sum(map(lambda x: 1 if x['first_url'] else 0, results))
+internet_count = sum(map(lambda x: 1 if x['speed_result']['download_speed'] != 'N/A' or x['first_url'] else 0, results))
 
 print(results)
-print(f'Invalid records total: {len(results) - ip_count} IPs, {len(results) - speed_count} download_speeds, {len(results) - search_count} urls')
+print(f'Invalid records total: {len(results) - ip_count} IPs, {len(results) - speed_count} working internet, {len(results) - speed_count} download_speeds, {len(results) - search_count} urls')
